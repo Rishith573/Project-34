e387aaa52dce6bae90f477cf1f701d3c41b36d77
Rishith573 <77111082+Rishith573@users.noreply.github.com>
2021-06-17 19:29:23 +0530

    Add files via upload

--- Hero.js
+++ Hero.js
@@ -1,14 +1,15 @@
 class Hero{
     constructor(x, y, width, height){
         var options = {
-            restitution = 0,
-            friction = 1,
-            isStatic = false
+         restitution : 0, 
+         friction : 1, 
+         isStatic : false 
         };
+
         this.body = Bodies.circle(x, y, width, height, options);
         this.width = width;
         this.height = height;
-        this.image = loadImage(images/hero.gif);
+        this.image = loadImage("images/hero.gif");
         World.add(world, this.body)
     }
 
